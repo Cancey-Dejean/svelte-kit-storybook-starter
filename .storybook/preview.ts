14690cf3987f20307e3c18a8bc0e919105ee1dee
Cancey-Dejean <canceyd@gmail.com>
2025-10-20 14:44:38 -0500

    Add card components and enhance Storybook configuration
    - Introduced new card components: `Card`, `CardContent`, `CardDescription`, `CardFooter`, `CardHeader`, `CardTitle`, and `CardAction`.
    - Updated Storybook preview configuration to include HTML parameters, viewport options, and decorators for theme management.
    - Refined global styles in `globals.css` by removing unnecessary whitespace.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,13 +4,34 @@ import { withThemeByClassName } from '@storybook/addon-themes';
 
 const preview: Preview = {
 	parameters: {
+		html: {
+      removeEmptyComments: true,
+    },
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
 				date: /Date$/i
 			}
-		}
-	}
+		},
+		options: {
+      storySort: {
+        method: "alphabetical",
+        order: [],
+      },
+    },
+		viewport: {
+      options: INITIAL_VIEWPORTS,
+    },
+	},
+	decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+  ],
 };
 
 export default preview;
